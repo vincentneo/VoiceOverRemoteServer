3570b1e12df648e250fe8d0aea168c106176636c
Vincent Neo <23420208+vincentneo@users.noreply.github.com>
2024-07-24 12:32:31 +0800

    Initial Commit

---
+++ TestVoiceOver/TestVoiceOverApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestVoiceOverApp.swift
+//  TestVoiceOver
+//
+//  Created by Vincent Neo on 24/7/24.
+//
+
+import SwiftUI
+
+@main
+struct TestVoiceOverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
